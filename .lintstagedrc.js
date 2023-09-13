c20bd03b03b7c3e89c81cad7b842dc64759d04ce
Thiago da Silva <s_th@live.com>
2023-09-13 10:00:48 -0300

    Initial jest tests and fix in files possiton

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,7 @@ module.exports = {
   // Lint & Prettify TS and JS files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `npm run lint --fix . ${filenames.join(' --file')}`,
-    `prettier --write ${filenames.join(' ')}`
+    `prettier --write ${filenames.join(' ')}`,
+    `npm test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
