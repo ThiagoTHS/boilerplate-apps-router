46c2af71e2b528abe54a7af5f51a57c3a40fc424
Thiago da Silva <s_th@live.com>
2023-09-14 09:42:15 -0300

    Add and config plop

---
+++ src/components/Baniu/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Baniu = () => (
+  <S.Wrapper>
+    <h1>Baniu</h1>
+  </S.Wrapper>
+)
+
+export default Baniu
---
+++ src/components/Baniu/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Baniu from '.'
+
+export default {
+  title: 'Baniu',
+  component: Baniu
+} as Meta
+
+export const Default: StoryObj = {}
