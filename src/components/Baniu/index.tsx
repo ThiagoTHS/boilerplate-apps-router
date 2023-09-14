0bf24a4c99763601c457a76ae957dcaf13dc7833
Thiago da Silva <s_th@live.com>
2023-09-14 09:51:51 -0300

    remove component exemplo

--- src/components/Baniu/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles'
-
-const Baniu = () => (
-  <S.Wrapper>
-    <h1>Baniu</h1>
-  </S.Wrapper>
-)
-
-export default Baniu
--- src/components/Baniu/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react'
-import Baniu from '.'
-
-export default {
-  title: 'Baniu',
-  component: Baniu
-} as Meta
-
-export const Default: StoryObj = {}
